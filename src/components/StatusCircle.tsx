6a4c90188f44cb4141e3b6b4c986090ce33b4c75
Bryan Mora <brm.988k@gmail.com>
2021-10-28 18:00:14 -0700

    Initial commit

--- src/components/StatusCircle.tsx
+++ src/components/StatusCircle.tsx
@@ -1,17 +1,19 @@
 import './styles/StatusCircle.css'
 
 enum Status {
-    UNAVAILABLE,
-    ASIGNED,
-    PENDING
+    UNAVAILABLE = "UNAVAILABLE", ASIGNED = "ASIGNED", PENDING = "PENDING"
 }
 
-export const StatusCircle = ({status }: Status) => {
+interface Props {
+    status: Status 
+}
+
+export const StatusCircle = ({ status }: Props) => {
+
     return (
-        <div id="circle" className={`mx-auto w-11/12 h-1/2 text-center border-8 border-${ props.UNAVAILABLE}-500`}>
+        <div id="circle" className={`mx-auto w-11/12 h-1/2 text-center border-8 border-${status}-500`}>
             <p className="font-bold">Asignación de <br />Servicio </p>
-            <span className="font-semibold">{"Hello"}</span>
+            <span className="font-semibold">{ Status.ASIGNED }</span>
         </div>
     )
-
 }
